88594175e109124872434d2133fdd39fd481186f
chenxing <1529274926@qq.com>
2019-04-30 14:00:00 +0800

    feat: 加入getter

---
+++ lombok-enchance/src/main/java/complie.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#创建保存class文件的文件夹
+if [ -d classes ]; then
+    rm -rf classes;
+fi
+mkdir classes
+
+#导入tools.jar，编译processor并输出
+javac -cp $JAVA_HOME/lib/tools.jar com/nicky/lombok/config/Getter*  com/nicky/lombok/annotation/Getter*  -d classes/
+
+#编译App.java，并使用javac的-processor参数指定编译阶段的处理器GetterProcessor
+javac -cp classes -d classes -processor com.nicky.lombok.config.GetterProcessor com/nicky/lombok/App.java
+
+#用javap显示编译后的App.class文件(非必须，方便看结果)
+javap -p classes classes/com/nicky/lombok/App.class
+
+#执行测试类
+java -cp classes com.nicky.lombok.App
